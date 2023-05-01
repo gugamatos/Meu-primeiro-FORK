156cc488b0c42f7923c1881fda0de40e0a3f2cc3
Guga Matos <gustavomaatos5@gmail.com>
2023-05-01 18:34:33 -0300

    Primeira atividade

---
+++ src/main.js
@@ -0,0 +1,22 @@
+Atividade Elabore um programa que leia um numero e devolva para o usuário o dobro do número informado.
+
+
+//valorDoNumero = 35;
+//let calculo = 0;
+//total = (valorDoNumero *=2);
+//console.log (total) 
+
+const valorDoNumero = prompt("valorDoNumero?")
+alert(valorDoNumero *2)
+
+
+// const valorDoJantar = prompt("valorDoJantar")
+// const taxaGarcom = prompt("taxaGarcom")
+// alert(valorDoJantar + taxaGarcom)
+
+//let valorDoJantar = 105;
+//let taxaGarcom = 10;
+//total = 0;
+//total = 100 + ((taxaGarcom / 100) * 100);
+
+
