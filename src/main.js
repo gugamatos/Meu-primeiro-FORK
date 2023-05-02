fc6ca3e0f405f6409dd6cd2cc627319a52f735b3
Guga Matos <gustavomaatos5@gmail.com>
2023-05-02 20:56:05 -0300

    Atividade concluida

--- src/main.js
+++ src/main.js
@@ -1,22 +1,27 @@
-Atividade Elabore um programa que leia um numero e devolva para o usuário o dobro do número informado.
+//Atividade Elabore um programa que leia um numero e devolva para o usuário o dobro do número informado.
 
-
-//valorDoNumero = 35;
-//let calculo = 0;
-//total = (valorDoNumero *=2);
-//console.log (total) 
 
 const valorDoNumero = prompt("valorDoNumero?")
 alert(valorDoNumero *2)
 
+ //Elabore um programa que calcule o valor do jantar, sendo o valor total do jantar composto pela conta junto com a taxa de atendimento do garçom. 
 
-// const valorDoJantar = prompt("valorDoJantar")
-// const taxaGarcom = prompt("taxaGarcom")
-// alert(valorDoJantar + taxaGarcom)
 
-//let valorDoJantar = 105;
-//let taxaGarcom = 10;
-//total = 0;
-//total = 100 + ((taxaGarcom / 100) * 100);
+
+const valorDoJantar = parseFloat (prompt("Valor do Jantar"));
+const taxaDoGarcom = parseFloat (prompt("Taxa do Garçom"));
+alert(valorDoJantar + taxaDoGarcom)
+
+//  
+// Elabore um programa para uma pizzaria, o qual leia o valor total de uma conta e quantos clientes vão pagá-la. Calcule e informe o valor a ser pago por cliente.
+
+
+const valorTotal = parseFloat (prompt("Valor da conta"));
+const quantidadeClientes = parseFloat (prompt("Quantos clientes vão pagar a conta?"));
+
+alert(valorTotal / quantidadeClientes)
+
+
+
 
 
